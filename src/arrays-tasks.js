8299dd9d8be09578280f09242e56610f5ffae40a
OlgaYurch <volha@yurchanka>
2024-01-27 13:38:24 +0300

    feat: add solution of task getIntervalArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,12 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  let array = [];
+  for (i = start; i < end; i += 1) {
+    array += 1;
+  }
+  return array;
 }
 
 /**
