ed7ae0169af8c2e6f1a1a1eb214aece1129dcb4f
OlgaYurch <volha@yurchanka>
2024-02-01 21:22:07 +0300

    feat: add solution of task findAllOccurrences

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -83,8 +83,9 @@ function findElement(arr, value) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  const countItem = arr.filter((element) => element === item);
+  return countItem.length;
 }
 
 /**
