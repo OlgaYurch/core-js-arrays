71125fc2b886e174a61fd8dac5daa4b6b156d1d9
Volha Yurchanka <volha@yurchanka.ru>
2025-10-19 10:21:32 +0300

    feat: add solution of task getFalsyValuesCount

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -392,8 +392,9 @@ function getElementByIndices(/* arr, indices */) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  const falsyValues = [false, 0, -0, 0n, '', null, undefined, NaN];
+  return arr.filter((value) => falsyValues.includes(value)).length;
 }
 
 /**
