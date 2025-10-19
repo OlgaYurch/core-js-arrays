63b2fe1e62a7fc230da1d1c79767f5e915a13c7d
Volha Yurchanka <volha@yurchanka.ru>
2025-10-19 16:23:23 +0300

    feat: add solution of task createChunks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -344,8 +344,16 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  return arr.reduce((result, item) => {
+    const lastChunk = result[result.length - 1];
+    if (!lastChunk || lastChunk.length === chunkSize) {
+      result.push([item]);
+    } else {
+      lastChunk.push(item);
+    }
+    return result;
+  }, []);
 }
 
 /**
